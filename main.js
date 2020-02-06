05cc637b49ca9845446b1c5f63a946806907d4ae
Lawrence Kim <lawrey7@gmail.com>
2020-02-06 10:18:12 -0800

    Added an instantiation of the PageHeader that is selecting the header element.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 var table = document.querySelector('table');
 var gradeTable = new GradeTable(table);
-var app = new App(gradeTable);
+var header = document.querySelector('header');
+var pageHeader = new PageHeader(header);
+var app = new App(gradeTable, pageHeader);
 app.start();
