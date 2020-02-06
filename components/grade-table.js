8c1669af8494f47b57e64771a9d99120a853e574
Lawrence Kim <lawrey7@gmail.com>
2020-02-05 17:58:51 -0800

    Changed the updateGrades method to receive the data from the API and update the table data and dynamically create rows to fit the data.

--- components/grade-table.js
+++ components/grade-table.js
@@ -3,6 +3,17 @@ class GradeTable {
     this.tableElement = tableElement;
   }
   updateGrades(grades) {
-    console.log(grades);
+    var tableBody = this.tableElement.querySelector('tbody');
+    tableBody.textContent = '';
+    for (var i = 0; i < 3; i++) {
+      var row = document.createElement('tr');
+      var name = row.appendChild(document.createElement('td'));
+      name.textContent = grades[i].name;
+      var course = row.appendChild(document.createElement('td'));
+      course.textContent = grades[i].course;
+      var grade = row.appendChild(document.createElement('td'));
+      grade.textContent = grades[i].grade;
+      tableBody.append(row);
+    }
   }
 }
