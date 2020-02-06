f73ef3494e34606f0717760d85c5d1ed22eae5c8
Lawrence Kim <lawrey7@gmail.com>
2020-02-06 10:26:48 -0800

    Changed the handleGetgradesSuccess method to get the average of the grades and pass the average as an argument to the updateAverage method of the pageHeader.

--- components/app.js
+++ components/app.js
@@ -4,6 +4,12 @@ class App {
   }
   handleGetGradesSuccess(grades) {
     this.gradeTable.updateGrades(grades);
+    var gradeAverage = 0;
+    for (var i = 0; i < grades.length; i++) {
+      gradeAverage += grades[i].grade;
+    }
+    gradeAverage = gradeAverage / grades.length;
+    this.pageHeader.updateAverage(gradeAverage);
   }
   constructor(gradeTable, pageHeader) {
     this.handleGetGradesError = this.handleGetGradesError.bind(this);
