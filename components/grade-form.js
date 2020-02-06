f119fde48dc6eb3f3c0697cfc4aa4925a210690f
Lawrence Kim <lawrey7@gmail.com>
2020-02-06 15:26:07 -0800

    Added new behavior to the handleSubmit method of the Gradeform class, to take the data from the form and pass it as an argument to a new method.

--- components/grade-form.js
+++ components/grade-form.js
@@ -8,7 +8,12 @@ class GradeForm {
     this.createGrade = createGrade;
   }
   handleSubmit(event) {
-    console.log('hi');
     event.preventDefault();
+    var formData = new FormData(event.target);
+    var name = formData.get('name');
+    var course = formData.get('course');
+    var grade = formData.get('grade');
+    this.createGrade(name, course, grade);
+    event.target.reset();
   }
 }
