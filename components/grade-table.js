7d611ed3611ca076e016dd9840ce5ea37f0b4cc7
Lawrence Kim <lawrey7@gmail.com>
2020-02-06 16:54:17 -0800

    Modified the updateGrades method to use the renderGradeRow method instead to have a delete button also added each row.

--- components/grade-table.js
+++ components/grade-table.js
@@ -6,14 +6,7 @@ class GradeTable {
     var tableBody = this.tableElement.querySelector('tbody');
     tableBody.textContent = '';
     for (var i = 0; i < grades.length; i++) {
-      var row = document.createElement('tr');
-      var name = row.appendChild(document.createElement('td'));
-      name.textContent = grades[i].name;
-      var course = row.appendChild(document.createElement('td'));
-      course.textContent = grades[i].course;
-      var grade = row.appendChild(document.createElement('td'));
-      grade.textContent = grades[i].grade;
-      tableBody.append(row);
+      this.renderGradeRow(grades[i], this.deleteGrade);
     }
   }
   onDeleteClick(deleteGrade) {
@@ -31,7 +24,8 @@ class GradeTable {
     var tableButtonData = row.appendChild(document.createElement('td'));
     var tableButton = tableButtonData.appendChild(document.createElement('button'));
     tableButton.textContent = 'DELETE';
-    tableButton.classList.add('btn btn-danger');
+    tableButton.classList.add('btn');
+    tableButton.classList.add('btn-danger');
     tableBody.append(row);
     tableButton.addEventListener('click', function () {
       deleteGrade(data.id);
